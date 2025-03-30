fb614582362a5ccf4b2d640063233f0fe790ec8f
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-03-31 00:41:34 +0100

    feat: add get started input button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="h-[104px] text-amber-50 flex px-24 justify-between items-center border-b border-b-[#048C80]">
+    <div className="h-24 sm:w-full text-amber-50 flex px-24 justify-between items-center border-b border-b-[#048C80]">
       <div className="flex items-center gap-2">
         <div className="flex items-center justify-center w-9 h-9 bg-gradient rounded-md">
           <img src="/logo.png" alt="Logo" />
@@ -9,9 +9,9 @@ export function Header() {
       </div>
 
       <div className="flex items-center gap-14 leading-6 text-[#737373]">
-        <p className="cursor-pointer hover:text-[#E7E7E7] text-sm">Features</p>
-        <p className="cursor-pointer hover:text-[#E7E7E7] text-sm">Pricing</p>
-        <p className="cursor-pointer hover:text-[#E7E7E7] text-sm">Support</p>
+        <p className="cursor-pointer hover:text-[#E7E7E7]">Features</p>
+        <p className="cursor-pointer hover:text-[#E7E7E7]">Pricing</p>
+        <p className="cursor-pointer hover:text-[#E7E7E7]">Support</p>
       </div>
 
         <button className="flex items-center gap-2 bg-gradient w-[167px] h-14 justify-center text-[#080808] rounded-md cursor-pointer">
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,15 +4,17 @@ export function HeroSection() {
       <div className="grid grid-cols-3 items-center">
         <div className="text-white col-span-2">
           <h1 className="font-bold text-6xl leading-[1.06] mb-4"><span className="text-[#048c80]">Intelligent</span> cloud-based <span className="text-[#048c80]">note-talking</span> and collaboration tool</h1>
-          <p className="mb-12">Experience the power of smart note-talking and transform the way you work today.</p>
+          <p className="mb-12 text-[#BABABA]">Experience the power of smart note-talking and transform the way you work today.</p>
 
-          <div className="p-10">
-            <input type="text" placeholder="Enter your email" />
-            <button>
-              <img src="/arrow.png" alt="Arrow" />
+          <div className="p-8.5 bg-white w-[434px] h-16 flex items-center rounded-lg relative">
+            <input type="text" placeholder="Enter your email" className="text-[#333] flex-grow outline-none pr-[140px]" />
+            <button className="flex bg-gradient px-6 py-4 rounded-lg items-center text-[#080808] whitespace-nowrap gap-2 cursor-pointer absolute right-2">
+              Sign up
+              <div className="flex items-center border rounded-sm border-[#0d0d0dad]">
+                <img src="/arrow.png" alt="Arrow" />
+              </div>
             </button>
           </div>
-          
         </div>
 
         <div className="flex justify-center items-center col-span-1">
